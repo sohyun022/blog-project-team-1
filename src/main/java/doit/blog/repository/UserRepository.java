8109250adc5d79f5ee1801e711d3aa1a548f1809
Sohyun Moon <sohyun22@ajou.ac.kr>
2024-11-13 18:31:01 +0900

    refactor: Use existsByUserLoginId instead of findByUserLoginId in UserRepository

--- src/main/java/doit/blog/repository/UserRepository.java
+++ src/main/java/doit/blog/repository/UserRepository.java
@@ -7,6 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByUserLoginId(String loginId);
-
+    boolean existsByUserLoginId(String loginId);
 }
