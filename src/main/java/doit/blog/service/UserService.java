ae0680f8c25d4f9c89f56758d6eb70ecad7a9d0c
Sohyun Moon <sohyun22@ajou.ac.kr>
2024-11-13 18:24:28 +0900

    refactor: Modify method name and return type

--- src/main/java/doit/blog/service/UserService.java
+++ src/main/java/doit/blog/service/UserService.java
@@ -4,6 +4,6 @@ import doit.blog.controller.user.dto.UserLoginRequest;
 import org.springframework.stereotype.Service;
 
 public interface UserService {
-    boolean isDuplicated(String id);
+    void validateDuplicateId(String id);
 
 }
