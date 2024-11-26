2ea6d013c149bd6ff0088d484375ea8e1f735eda
Sohyun Moon <sohyun22@ajou.ac.kr>
2024-11-27 01:17:28 +0900

    feat: enhance UserInfoResponse with static factory method
    - Added a static `from(User user)` method for convenient DTO creation

--- src/main/java/doit/blog/controller/user/dto/UserInfoResponse.java
+++ src/main/java/doit/blog/controller/user/dto/UserInfoResponse.java
@@ -1,7 +1,10 @@
 package doit.blog.controller.user.dto;
 
+import doit.blog.controller.user.domain.User;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
 
+@Builder
 public record UserInfoResponse(
         @Schema(description = "유저 식별 ID", example = "1")
         Long userId,
@@ -18,4 +21,13 @@ public record UserInfoResponse(
         @Schema(description = "전화번호", example = "010-1234-5678")
         String userPhoneNumber
 ) {
+        public static UserInfoResponse from(User user){
+                return UserInfoResponse.builder()
+                        .userId(user.getId())
+                        .userLoginId(user.getUserLoginId())
+                        .userName(user.getUserName())
+                        .userNickname(user.getUserNickname())
+                        .userPhoneNumber(user.getUserPhoneNumber())
+                        .build();
+        }
 }
