efd1f461ac1ad3a2464b2a8bcfd37bc9ccd33aa4
Sohyun Moon <sohyun22@ajou.ac.kr>
2024-11-27 01:25:51 +0900

    feat: add login and getUserInfo methods to UserService with PasswordEncoder for secure password handling

--- src/main/java/doit/blog/service/UserService.java
+++ src/main/java/doit/blog/service/UserService.java
@@ -1,10 +1,13 @@
 package doit.blog.service;
 
+import doit.blog.controller.user.dto.UserIdResponse;
+import doit.blog.controller.user.dto.UserInfoResponse;
+import doit.blog.controller.user.dto.UserLoginRequest;
 import doit.blog.controller.user.dto.UserSignUpRequest;
 
 public interface UserService {
-    void validateDuplicateId(String id);
-
-    void signUp(UserSignUpRequest userSignUpRequest);
-
+    void validateDuplicateId(String userLoginId);
+    UserIdResponse signUp(UserSignUpRequest userSignUpRequest);
+    UserIdResponse login(UserLoginRequest userLoginRequest);
+    UserInfoResponse getUserInfo(Long userId);
 }
--- src/main/java/doit/blog/service/UserServiceImpl.java
+++ src/main/java/doit/blog/service/UserServiceImpl.java
@@ -1,21 +1,28 @@
 package doit.blog.service;
 
 import doit.blog.controller.user.domain.User;
+import doit.blog.controller.user.dto.UserIdResponse;
+import doit.blog.controller.user.dto.UserInfoResponse;
+import doit.blog.controller.user.dto.UserLoginRequest;
 import doit.blog.controller.user.dto.UserSignUpRequest;
 import doit.blog.exception.CustomErrorInfo;
 import doit.blog.exception.CustomException;
 import doit.blog.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
+@Transactional
 public class UserServiceImpl implements UserService {
 
+    private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
-    public UserServiceImpl(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
 
     @Override
+    @Transactional(readOnly = true)
     public void validateDuplicateId(String userLoginId){
         if (userRepository.existsByUserLoginId(userLoginId)) {
             throw new CustomException(CustomErrorInfo.ID_DUPLICATION);
@@ -23,20 +30,27 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public void signUp(UserSignUpRequest userSignUpRequest){
-
+    public UserIdResponse signUp(UserSignUpRequest userSignUpRequest){
         validateDuplicateId(userSignUpRequest.userLoginId());
-
-        User user = User.builder()
-                .userName(userSignUpRequest.userName())
-                .userNickname(userSignUpRequest.userNickname())
-                .userPassword(userSignUpRequest.userPassword())
-                .userLoginId(userSignUpRequest.userLoginId())
-                .userPhoneNumber(userSignUpRequest.userPhoneNumber())
-                .build();
-
+        User user = User.create(userSignUpRequest,passwordEncoder);
         userRepository.save(user);
+        return UserIdResponse.from(user);
     }
 
+    @Override
+    public UserIdResponse login(UserLoginRequest userLoginRequest){
+        User user = userRepository.findByUserLoginId(userLoginRequest.userLoginId());
+        if (user == null || !passwordEncoder.matches(userLoginRequest.userPassword(), user.getUserPassword())) {
+            throw new CustomException(CustomErrorInfo.INVALID_ID_OR_PASSWORD);
+        }
+        return UserIdResponse.from(user);
+    }
 
+    @Override
+    @Transactional(readOnly = true)
+    public UserInfoResponse getUserInfo(Long userId){
+        User user = userRepository.findById(userId)
+                .orElseThrow(()->new CustomException(CustomErrorInfo.USER_NOT_FOUND));
+        return UserInfoResponse.from(user);
+    }
 }
