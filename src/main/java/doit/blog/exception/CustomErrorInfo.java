4d452aae62b4dad98e2bdf46ee865ab264dd790a
Sohyun Moon <sohyun22@ajou.ac.kr>
2024-11-27 00:41:03 +0900

    feat: update and add error codes in CustomErrorInfo

--- src/main/java/doit/blog/exception/CustomErrorInfo.java
+++ src/main/java/doit/blog/exception/CustomErrorInfo.java
@@ -6,17 +6,19 @@ import lombok.Getter;
 public enum CustomErrorInfo {
 
     // 400 BAD_REQUEST
-    INVALID_PASSWORD_FORMAT(40, "올바른 비밀번호 형식이 아닙니다.", 400001),
+    INVALID_PASSWORD_FORMAT(400, "올바른 비밀번호 형식이 아닙니다.", 400001),
+    INVALID_ID_OR_PASSWORD(400,"아이디 또는 비밀번호가 잘못되었습니다.",400002),
+    ID_DUPLICATION(400, "중복된 아이디입니다.", 400003),
 
     // 401 UNAUTHORIZED
 
     // 403 FORBIDDEN
 
     // 404 NOT_FOUND
-    MEMBER_NOT_FOUND(404, "존재하지 않는 멤버입니다.", 404001),
+    USER_NOT_FOUND(404, "존재하지 않는 멤버입니다.", 404001),
 
     // 409 CONFLICT
-    ID_DUPLICATION(409, "중복된 아이디입니다.", 409001),
+
 
     // 500 INTERNAL_SERVER_ERROR
     INTERNAL_SERVER_ERROR(500, "서버 내부 오류", 500001);
