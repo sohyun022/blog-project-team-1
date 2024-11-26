97f8e79686e9e9559ffebe29c0c0dac7e06afba2
Sohyun Moon <sohyun22@ajou.ac.kr>
2024-11-27 01:16:41 +0900

    feat: enhance UserIdResponse with static factory method
    - Added a static `from(User user)` method for convenient DTO creation

--- src/main/java/doit/blog/controller/user/dto/UserIdResponse.java
+++ src/main/java/doit/blog/controller/user/dto/UserIdResponse.java
@@ -1,9 +1,13 @@
 package doit.blog.controller.user.dto;
 
+import doit.blog.controller.user.domain.User;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record UserIdResponse(
         @Schema(description = "유저 식별 ID", example = "1")
         Long userId
 ) {
+        public static UserIdResponse from(User user){
+                return new UserIdResponse(user.getId());
+        }
 }
